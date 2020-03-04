b8b636db7eb7be689d52186df0c6a28b80195849
Ankit Shah <ankitshah123440@gmail.com>
2020-03-04 23:22:49 +0530

    Updated URL in the movie catalog controller as we've added zuul server.So URLs will be changed.

--- MovieCatalogService/src/main/java/com/moviecatalog/controller/MovieController.java
+++ MovieCatalogService/src/main/java/com/moviecatalog/controller/MovieController.java
@@ -1,6 +1,7 @@
 package com.moviecatalog.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,14 +10,22 @@ import org.springframework.web.bind.annotation.RestController;
 import com.moviecatalog.service.MovieCatalogService;
 
 @RestController
-@RequestMapping("/movie")
+//@RequestMapping("/movie")
+@RequestMapping("/")
 public class MovieController {
-	
+
 	@Autowired
 	MovieCatalogService movieCatalogService;
 
-	@GetMapping(value = "/{movieName}",produces = "application/json")
+	@Autowired
+	Environment env;
+
+	// Upadated URL as we have added zuul api gateway so we are checking /movie at gateway level so we have
+	// to remove /movie from here.
+//	@GetMapping(value = "/{movieName}", produces = "application/json")
+	@GetMapping(value = "{movieName}", produces = "application/json")
 	public String getMovieDetailsByName(@PathVariable("movieName") String movieName) {
+		System.out.println(env.getProperty("local.server.port"));
 		return movieCatalogService.getCompleteMovieDetails(movieName);
 	}
 }
